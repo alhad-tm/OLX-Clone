55a2eceb8ed454669516fa2d2f33d7771519a73b
alhad-tm <alhadchikkutm@gmail.com>
2022-12-18 03:17:10 +0530

    setup createcontext for view post

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router,Route } from 'react-router-dom';
 import Signup from './Pages/Signup'
 import Login from './Pages/Login'
 import Create from './Pages/Create'
+import ViewPost from "./Pages/ViewPost"
 import './App.css';
 import { AuthContext, FirebaseContext } from './store/Context';
+import Post from './store/PostContext';
 
 /**
  * ?  =====Import Components=====
@@ -21,20 +23,22 @@ function App() {
   })
   return (
     <div>
+   <Post> 
+
       <Router>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/signup">
-          <Signup />
-        </Route>
-        <Route path="/login">
-          <Login/>
-        </Route>
-        <Route path="/create">
-          <Create/>
-        </Route>
+        
+        <Route path="/" exact><Home /> </Route>
+
+        <Route path="/signup"><Signup /></Route>
+
+        <Route path="/login"><Login/></Route>
+
+        <Route path="/create"> <Create/> </Route>
+
+        <Route path="/view"> <ViewPost/> </Route>
+
       </Router>
+  </Post>  
       
     </div>
   );
